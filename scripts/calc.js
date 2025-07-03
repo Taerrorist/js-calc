93b7b11da9f440c0959a00f8bad60dc9a8f465a1
Is an English Major <Najeebihesa@gmail.com>
2025-07-03 09:02:50 +0300

    js calc homework

---
+++ scripts/calc.js
@@ -0,0 +1,21 @@
+const display = document.getElementById("display");
+
+function add(numOne, numTwo) {
+    return numOne + numTwo
+}
+function deduct(numOne, numTwo){
+return numOne - numTwo
+}
+function multiply(numOne, numTwo) {
+    return numOne * numTwo
+}
+function divide(numOne, numTwo){
+    return numOne / numTwo
+}
+
+function calculate(){
+
+}
+
+
+//MESSAGE TO INSTRUCTORS: I am struggling quite a lot in javascript, please help me undestand it, please get me out of javascript hell.
